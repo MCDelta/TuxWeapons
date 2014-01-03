633ac13bac2b68d4f2377fd35eff586f042d12f9
TuxCraft <tux.craft.42@gmail.com>
2014-01-02 20:05:54 -0500

    Added two files

---
+++ common/mcdelta/tuxweapons/PlayerData.java
@@ -0,0 +1,33 @@
+package mcdelta.tuxweapons;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class PlayerData
+{
+     public NBTTagCompound tag;
+     private EntityPlayer  player;
+     
+     
+     
+     
+     public PlayerData (EntityPlayer player)
+     {
+          this.player = player;
+          this.tag = player.getEntityData().getCompoundTag("MCDeltaInfo");
+          
+          if (this.tag == null)
+          {
+               this.tag = new NBTTagCompound();
+               save();
+          }
+     }
+     
+     
+     
+     
+     public void save ()
+     {
+          this.player.getEntityData().setCompoundTag("MCDeltaInfo", this.tag);
+     }
+}
---
+++ common/mcdelta/tuxweapons/TWNBTTags.java
@@ -0,0 +1,13 @@
+package mcdelta.tuxweapons;
+
+public class TWNBTTags
+{
+     
+     public static final String POTION_EFFECTS      = "CustomPotionEffects";
+     public static final String REDSTONE_USED       = "RedstonePrevUsed";
+     public static final String PLAYER_SHIELD       = "OffhandWeapon";
+     public static final String RENDER_AS_SECONDARY = "SecondaryHand";
+     public static final String GRAPP               = "GrappHookID";
+     public static final int    SPLASH_ID           = 16400;
+     
+}
